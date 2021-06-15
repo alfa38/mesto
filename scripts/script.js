6cf2bf81d0dc8afb692af1ea554a7a439f2e3e35
alfa38 <alpharium89@gmail.com>
2021-06-15 19:24:23 +0300

    Pract 5, rewiev #1 changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,129 +1,123 @@
 const initialCards = [
     {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+        name: "Архыз",
+        link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg"
     },
     {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+        name: "Челябинская область",
+        link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg"
     },
     {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+        name: "Иваново",
+        link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg"
     },
     {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+        name: "Камчатка",
+        link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg"
     },
     {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+        name: "Холмогорский район",
+        link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg"
     },
     {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+        name: "Байкал",
+        link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg"
     }
-  ];
+];
 
-let cardsContainer = document.querySelector('.cards-container');
+const cardsContainer = document.querySelector(".cards-container");
 
-let addCardButton = document.querySelector('.profile__button_type_add-card');
-let addCardOverlay = document.querySelector('#modal-add-new-card');
-let addCardNameInput = addCardOverlay.querySelector('.edit-form__input_edit_name');
-let addCardSourceInput = addCardOverlay.querySelector('.edit-form__input_edit_img-source');
-let closeAddNewCardButton = addCardOverlay.querySelector('.modal-overlay__button_type_close-modal');
-let addNewCardForm = addCardOverlay.querySelector('.edit-form');
+const addCardButton = document.querySelector(".profile__button_type_add-card");
+const addCardOverlay = document.querySelector("#modal-add-new-card");
+const addCardNameInput = addCardOverlay.querySelector(".edit-form__input_edit_name");
+const addCardSourceInput = addCardOverlay.querySelector(".edit-form__input_edit_img-source");
+const closeAddNewCardButton = addCardOverlay.querySelector(".modal-overlay__button_type_close-modal");
+const addNewCardForm = addCardOverlay.querySelector(".edit-form");
 
-let editProfileButton = document.querySelector('.profile__button_type_edit-profile');
-let editProfileOverlay = document.querySelector('#modal-edit-profile');
-let modalOverlayNameInput = editProfileOverlay.querySelector('.edit-form__input_edit_name');
-let modalOverlayProfessionInput = editProfileOverlay.querySelector('.edit-form__input_edit_profession');
-let closeEditProfileButton = editProfileOverlay.querySelector('.modal-overlay__button_type_close-modal');
-let saveProfileSettingForm = editProfileOverlay.querySelector('.edit-form');
+const editProfileButton = document.querySelector(".profile__button_type_edit-profile");
+const editProfileOverlay = document.querySelector("#modal-edit-profile");
+const modalOverlayNameInput = editProfileOverlay.querySelector(".edit-form__input_edit_name");
+const modalOverlayProfessionInput = editProfileOverlay.querySelector(".edit-form__input_edit_profession");
+const closeEditProfileButton = editProfileOverlay.querySelector(".modal-overlay__button_type_close-modal");
+const saveProfileSettingForm = editProfileOverlay.querySelector(".edit-form");
 
-let photoViewierOverlay = document.querySelector('#modal-photo-viewier');
-let closePhotoViewierButton = photoViewierOverlay.querySelector('.modal-overlay__button_type_close-modal');
+const photoViewierOverlay = document.querySelector("#modal-photo-viewier");
+const closePhotoViewierButton = photoViewierOverlay.querySelector(".modal-overlay__button_type_close-modal");
+const photoViewierImage = photoViewierOverlay.querySelector(".photo-viewier__image");
+const photoViewierCaption = photoViewierOverlay.querySelector(".photo-viewier__caption");
 
+const profileName = document.querySelector(".profile__name");
+const profileProfession = document.querySelector(".profile__profession");
 
-let profileName = document.querySelector('.profile__name');
-let profileProfession = document.querySelector('.profile__profession');
+const templateCard = document.querySelector("#card-item-template").content;
 
-let setLike = (button) => () => button.classList.toggle('card-item__button_active');
-let removeCard = (card) => () => card.remove();
+const setLike = (button) => () => button.classList.toggle("card-item__button_active");
+const removeCard = (card) => () => card.remove();
 
-let addNewCard = (name, link) => {
-    let templateCard = document.querySelector('#card-item-template').content;
-    let newCard = templateCard.querySelector('.card-item').cloneNode(true);
-    let likeButton = newCard.querySelector('.card-item__button_type_set-like');
-    let removeButton = newCard.querySelector('.card-item__button_type_remove-card');
-    let cardImage = newCard.querySelector('.card-item__image');
+const createCard = (name, link) => {
+    const newCard = templateCard.querySelector(".card-item").cloneNode(true);
+    const likeButton = newCard.querySelector(".card-item__button_type_set-like");
+    const removeButton = newCard.querySelector(".card-item__button_type_remove-card");
+    const cardImage = newCard.querySelector(".card-item__image");
 
-    newCard.querySelector('.card-item__name').textContent = name;
-    newCard.querySelector('.card-item__image').setAttribute('src', link);
-    newCard.querySelector('.card-item__image').setAttribute('alt', `Картинка "${name}"`);
+    newCard.querySelector(".card-item__name").textContent = name;
+    cardImage.setAttribute("src", link);
+    cardImage.setAttribute("alt", `Картинка "${name}"`);
 
-    likeButton.addEventListener('click', setLike(likeButton));
-    removeButton.addEventListener('click', removeCard(newCard));
-    cardImage.addEventListener('click', createOpenPhotoViewierEventListener(name, link));
+    likeButton.addEventListener("click", setLike(likeButton));
+    removeButton.addEventListener("click", removeCard(newCard));
+    cardImage.addEventListener("click", () => openPhotoViewierModal(name, link));
+    return newCard;
+};
 
-    cardsContainer.prepend(newCard);
-}
+const addNewCard = (name, link) => {
+    cardsContainer.prepend(createCard(name, link));
+};
 
-let closeEditProfileModal = () => {
-    editProfileOverlay.classList.remove('modal-overlay_open');
-}
+const openModal = (modal) => {
+    modal.classList.add("modal-overlay_open");
+};
+const closeModal = (modal) => {
+    modal.classList.remove("modal-overlay_open");
+};
 
-let closeAddNewCardModal = () => {
-    addNewCardForm.reset();
-    addCardOverlay.classList.remove('modal-overlay_open');
-}
+const openEditProfileModal = () => {
+    openModal(editProfileOverlay);
+    modalOverlayNameInput.value = profileName.textContent ?? "";
+    modalOverlayProfessionInput.value = profileProfession.textContent ?? "";
+};
 
-let closePhotoViewierModal = () => {
-    photoViewierOverlay.classList.remove('modal-overlay_open');
-}
+const openPhotoViewierModal = (name, link) => {
+    photoViewierImage.setAttribute("src", link);
+    photoViewierImage.setAttribute("alt", name);
+    photoViewierCaption.textContent = name;
+    openModal(photoViewierOverlay);
+};
 
-let openEditProfileModal = () => {
-    editProfileOverlay.classList.add('modal-overlay_open');
-    modalOverlayNameInput.setAttribute('value', profileName.textContent ?? '');
-    modalOverlayProfessionInput.setAttribute('value', profileProfession.textContent ?? '');
-}
-
-let openAddNewCardModal = () => {
-    addCardOverlay.classList.add('modal-overlay_open');
-}
-
-let createOpenPhotoViewierEventListener = (name, link) => () => openPhotoViewierModal(name, link);
-
-let openPhotoViewierModal = (name, link) => {
-    photoViewierOverlay.querySelector('.photo-viewier__image').setAttribute('src', link);
-    photoViewierOverlay.querySelector('.photo-viewier__image').setAttribute('alt', name);
-    photoViewierOverlay.querySelector('.photo-viewier__caption').textContent = name;
-    photoViewierOverlay.classList.add('modal-overlay_open');
-}
-
-let saveChanges = (event) => {
+const saveChanges = (event) => {
     event.preventDefault();
     profileName.textContent = modalOverlayNameInput.value;
     profileProfession.textContent = modalOverlayProfessionInput.value;
-    closeEditProfileModal();
-}
+    closeModal(editProfileOverlay);
+};
 
-let addNewCardFromModal = (event) => {
+const addNewCardFromModal = (event) => {
     event.preventDefault();
     addNewCard(addCardNameInput.value, addCardSourceInput.value);
-    closeAddNewCardModal();
-}
+    addNewCardForm.reset();
+    closeModal(addCardOverlay);
+};
+
+editProfileButton.addEventListener("click", openEditProfileModal);
+addCardButton.addEventListener("click", () => openModal(addCardOverlay));
+
+closeEditProfileButton.addEventListener("click", () => closeModal(editProfileOverlay));
+closeAddNewCardButton.addEventListener("click", () => closeModal(addCardOverlay));
+closePhotoViewierButton.addEventListener("click", () => closeModal(photoViewierOverlay));
+
+saveProfileSettingForm.addEventListener("submit", saveChanges);
+addNewCardForm.addEventListener("submit", addNewCardFromModal);
 
 initialCards.forEach((cardData) => {
     addNewCard(cardData.name, cardData.link);
 });
-
-editProfileButton.addEventListener('click', openEditProfileModal);
-addCardButton.addEventListener('click', openAddNewCardModal);
-
-closeEditProfileButton.addEventListener('click', closeEditProfileModal);
-closeAddNewCardButton.addEventListener('click', closeAddNewCardModal);
-closePhotoViewierButton.addEventListener('click', closePhotoViewierModal);
-
-saveProfileSettingForm.addEventListener('submit', saveChanges);
-addNewCardForm.addEventListener('submit', addNewCardFromModal);
